3dc4b25ddb37df7aa89f69e2262f58a8bcbe8bef
Shloka20 <shlokanayan@gmail.com>
2024-08-12 02:32:11 +0530

    Add files via upload

---
+++ 1st repository R.R
@@ -0,0 +1,24 @@
+Number_of_patients<-2
+P1_name<-"Shraddha"
+P2_name<-"Khushi"
+P1_age<- 22
+P2_age<- 25
+P1_weight<- 57.4
+P2_weight<- 65.3
+Both_girls<- TRUE
+typeof(Number_of_patients)
+type.convert(integer(P1_weight))
+as.character((P1_weight))
+as.integer(P1_weight)
+as.numeric(P1_weight)
+as.integer(P2_weight)
+as.numeric(P2_weight)
+as.character(P1_name)
+as.character(P2_name)
+as.logical(Both_girls)
+as.numeric(P1_age)
+as.integer(P2_age)
+typeof(P1_name)
+typeof(P1_age)
+typeof(P1_weight)
+typeof(Both_girls)
